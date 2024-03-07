f6c33e8db4ddcc8ca37c2d5152c944238a827552
wilson048 <therealsponge7@gmail.com>
2024-03-07 01:23:48 -0600

    Move All Index Page Data to JSON

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-recentCarousel = document.getElementById("Recent-Carousel");
+let recentCarousel = document.getElementById("Recent-Carousel");
 recentCarousel.innerHTML = 
-    `<div class="carousel-indicators">
+  `<div class="carousel-indicators">
     <button type="button" data-bs-target="#Recent-Carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#Recent-Carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#Recent-Carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -9,30 +9,24 @@ recentCarousel.innerHTML =
     <div class="carousel-item active">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption text-start">
-          <h1>Example headline.</h1>
-          <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+        <div id = "Recent-1" class="carousel-caption text-start">
+          
         </div>
       </div>
     </div>
     <div class="carousel-item">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Some representative placeholder content for the second slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+        <div id = "Recent-2" class="carousel-caption text-start">
+          
         </div>
       </div>
     </div>
     <div class="carousel-item">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption text-end">
-          <h1>One more for good measure.</h1>
-          <p>Some representative placeholder content for the third slide of this carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+        <div id = "Recent-3" class="carousel-caption text-start">
+          
         </div>
       </div>
     </div>
@@ -46,9 +40,9 @@ recentCarousel.innerHTML =
     <span class="visually-hidden">Next</span>
   </button>`
 
-historyCarousel = document.getElementById("History-Carousel");
+let historyCarousel = document.getElementById("History-Carousel");
 historyCarousel.innerHTML = 
-    `<div class="carousel-indicators">
+  `<div class="carousel-indicators">
     <button type="button" data-bs-target="#History-Carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#History-Carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#History-Carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -57,30 +51,24 @@ historyCarousel.innerHTML =
     <div class="carousel-item active">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption text-start">
-          <h1>Example headline.</h1>
-          <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+        <div id = "History-1" class="carousel-caption text-end">
+          
         </div>
       </div>
     </div>
     <div class="carousel-item">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Some representative placeholder content for the second slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+        <div id = "History-2" class="carousel-caption text-end">
+          
         </div>
       </div>
     </div>
     <div class="carousel-item">
       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
       <div class="container">
-        <div class="carousel-caption text-end">
-          <h1>One more for good measure.</h1>
-          <p>Some representative placeholder content for the third slide of this carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+        <div id = "History-3" class="carousel-caption text-end">
+          
         </div>
       </div>
     </div>
@@ -94,24 +82,92 @@ historyCarousel.innerHTML =
     <span class="visually-hidden">Next</span>
   </button>`
 
-fetch('wvchu_Activity08_MoviesFromJSON.json')
+fetch('data.json')
     .then(response => response.json())
     .then(data => placeIndexData(data));
 
-function placeIndexData(data) {
-  var mainContainer = document.getElementById("goodmovies");
+function placeIndexData(jsonData) {
+  // Top of Screen data
+  // Get parent and create children elements
+  var top_element = document.getElementById("top-of-page");
+  var h2TopElement = document.createElement("h2");
+  var pTopElement = document.createElement("p");
+  // Populate Elements
+  h2TopElement.className = "featurette-heading fw-normal lh-1";
+  h2TopElement.innerText = jsonData.top_page.text;
+  pTopElement.className = "lead";
+  pTopElement.innerText = jsonData.top_page.subtext;
+  // Append to parent
+  top_element.appendChild(h2TopElement);
+  top_element.appendChild(pTopElement);
+  // IFrame for top of screen
+  // Get parent and create children elements
+  var top_iframe_element = document.getElementById("iframe");
+  var iFrameTop = document.createElement("iframe");
+  // Add HTML properties
+  iFrameTop.src = jsonData.top_page.iframe_link;
+  iFrameTop.width = 500;
+  iFrameTop.height = 500;
+  iFrameTop.style = `border:0;`;
+  iFrameTop.xmlns = "http://www.w3.org/2000/svg";
+  // Append to parent
+  top_iframe_element.appendChild(iFrameTop);
 
-  for(var i = 0; i < data.movies.length; i++) {
-      let title = data.movies[i].title;
-      let year = data.movies[i].year;
-      let img = data.movies[i].url;
-
-      console.log("Title " + i + ": " + title + " Year: " + year);
-
-      let div = document.createElement("div");
-      div.innerHTML = `<h3> ${title} </h3>
-      <br> Year: ${year} 
-      <br> <img src = ${img} width="200"> <br> <br>`;
-      mainContainer.appendChild(div);
+  // Carousel Data
+  for(var i = 0; i < jsonData.index_recent_data.length; i++) {
+      // First Carousel, featuring recent information 
+      // Get parent element + create 3 new elements
+      var recentElement = document.getElementById("Recent-" + (i + 1));
+      var hRecentElement = document.createElement("h1");
+      var pRecentElementOne = document.createElement("p");
+      var pRecentElementTwo = document.createElement("p");
+      // Populate elements with data from data.json
+      hRecentElement.innerText = jsonData.index_recent_data[i].text;
+      pRecentElementOne.innerText = jsonData.index_recent_data[i].subtext;
+      pRecentElementTwo.innerHTML = `<a class="btn btn-lg btn-primary" href="#">${jsonData.index_recent_data[i].btntext}</a>`
+      // Append elements to parent element
+      recentElement.appendChild(hRecentElement);
+      recentElement.appendChild(pRecentElementOne);
+      recentElement.appendChild(pRecentElementTwo);
+      // Second Carousel, featuring historic information 
+      // Get parent element + create 3 new elements
+      var historyElement = document.getElementById("History-" + (i + 1));
+      var hHistoryElement = document.createElement("h1");
+      var pHistoryElementOne = document.createElement("p");
+      var pHistoryElementTwo = document.createElement("p");
+      // Populate elements with data from data.json
+      hHistoryElement.innerText = jsonData.index_history_data[i].text;
+      pHistoryElementOne.innerText = jsonData.index_history_data[i].subtext;
+      pHistoryElementTwo.innerHTML = `<a class="btn btn-lg btn-primary" href="#">${jsonData.index_history_data[i].btntext}</a>`
+      // Append elements to parent element
+      historyElement.appendChild(hHistoryElement);
+      historyElement.appendChild(pHistoryElementOne);
+      historyElement.appendChild(pHistoryElementTwo);
   }
+
+  // Bottom Page Data
+  var bottomElement = document.getElementById("bottom-page");
+  var h2BottomElement = document.createElement("h2");
+  var pBottomElement = document.createElement("p");
+  
+  h2BottomElement.className = "featurette-heading fw-normal lh-1";
+  h2BottomElement.innerText = jsonData.bottom_page.text;
+  pBottomElement.className = "lead";
+  pBottomElement.innerText = jsonData.bottom_page.subtext;
+  
+  bottomElement.append(h2BottomElement);
+  bottomElement.append(pBottomElement);
+  // Bottom Page Image Data
+  var bottomImgElement = document.getElementById("bottom-page-img");
+  var bottomImg = document.createElement("img");
+  bottomImg.src = jsonData.bottom_page.img;
+  bottomImg.className = "bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto";
+  bottomImg.width = 500;
+  bottomImg.height = 500;
+  bottomImg.xmlns = "http://www.w3.org/2000/svg";
+  bottomImg.role = "img";
+  bottomImg.preserveAspectRatio = "xMidYMid slice";
+  bottomImg.focusable = "false";
+
+  bottomImgElement.appendChild(bottomImg);
 }
