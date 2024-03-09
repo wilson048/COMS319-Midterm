6c284fb316432332dcfd84b244b598efe839a082
wilson048 <therealsponge7@gmail.com>
2024-03-09 16:45:53 -0600

    Added Building Drop Down Functionaility

--- floorplans.js
+++ floorplans.js
@@ -1,23 +1,69 @@
 fetch('data.json')
     .then(response => response.json())
-    .then(data => placeFloorplanData(data));
-let floorData = [];
-let hallData = [];
-function placeFloorplanData(jsonData) {
+    .then(data => placeDropdownData(data));
+
+function placeDropdownData(jsonData) {
+    // Build Dropdown menu for buildings
     buildingDropdown = document.getElementById("Building-Dropdown");
     for(var i = 0; i < jsonData.building_dropdown.length; i++) {
-        var lelement = document.createElement("li");
-        lelement.innerHTML = `<a class="dropdown-item" href="#">${jsonData.building_dropdown[i].text}</a>`;
-        buildingDropdown.appendChild(lelement);
+        var lElement = document.createElement("li");
+        var lButtonElement = document.createElement("a");
+        lButtonElement.className = "dropdown-item";
+        lButtonElement.id = jsonData.building_dropdown[i].text;
+        lButtonElement.innerText = jsonData.building_dropdown[i].text;
+        lButtonElement.addEventListener("click", function() {
+            placeBuildingData(this.id, jsonData);
+        })
+        // lElement.innerHTML = `<a class="dropdown-item" href="#">${}</a>`;
+        lElement.appendChild(lButtonElement);
+        buildingDropdown.appendChild(lElement);
     }
-    var floorsDropdown = document.getElementById("Floor-Dropdown");
-    if(wilson) {
+}
 
+function placeBuildingData(text, jsonData) {
+    var buildingDiv = document.getElementById("Building");
+    buildingDiv.innerHTML = '';
+    var h1Element = document.createElement("h1");
+    var pElement = document.createElement("p");
+    var imgElement = document.createElement("img");
+    var isSelected = false;
+    // Cases for changing Images 
+    switch (text) {
+        // Wallace
+        case "Wallace":
+            h1Element.innerText = jsonData.building_dropdown[0].text;
+            pElement.innerText = jsonData.building_dropdown[0].address;
+            imgElement.src = jsonData.building_dropdown[0].img;
+            isSelected = true;
+            break;
+        // Wilson
+        case "Wilson":
+            h1Element.innerText = jsonData.building_dropdown[1].text;
+            pElement.innerText = jsonData.building_dropdown[1].address;
+            imgElement.src = jsonData.building_dropdown[1].img;
+            isSelected = true;
+            break;
+        // Commons
+        case "Commons":
+            h1Element.innerText = jsonData.building_dropdown[2].text;
+            pElement.innerText = jsonData.building_dropdown[2].address;
+            imgElement.src = jsonData.building_dropdown[2].img;
+            isSelected = true;
+            break;
+        default:
+            break;
     }
-    else if (wallace) {
+    // Load Data into div element if a button is selected
+    if(isSelected) {
+        buildingDiv.appendChild(h1Element);
+        buildingDiv.appendChild(pElement);
+        buildingDiv.appendChild(imgElement);
+    }
+    // <!-- <h1>Wallace</h1>
+    // <p>917 Welch Ave, Ames IA</p>
+    // <img src="images/night towers photo.jpg"> -->
+}
 
-    }
-    else if (commons) {
+function placeFloorplanData() {
 
-    }
 }
