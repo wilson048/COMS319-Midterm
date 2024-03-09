fc2e51e3e4c5541341f324ae5d132b9d8fc4df76
antzmon <antkord@iastate.edu>
2024-03-09 16:01:52 -0600

    added to list js

--- floorplans.js
+++ floorplans.js
@@ -7,8 +7,17 @@ function placeFloorplanData(jsonData) {
     buildingDropdown = document.getElementById("Building-Dropdown");
     for(var i = 0; i < jsonData.building_dropdown.length; i++) {
         var lelement = document.createElement("li");
-        lelement.innerHTML = `<a class="dropdown-item" href="#">Action</a>`;
+        lelement.innerHTML = `<a class="dropdown-item" href="#">${jsonData.building_dropdown[i].text}</a>`;
         buildingDropdown.appendChild(lelement);
     }
-    
+    var floorsDropdown = document.getElementById("Floor-Dropdown");
+    if(wilson) {
+
+    }
+    else if (wallace) {
+
+    }
+    else if (commons) {
+
+    }
 }
